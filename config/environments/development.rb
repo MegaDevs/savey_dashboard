96f8f2ca5c446a3d75ace71024afe7f772b04b89
sirnicolaz <sirnicolaz@gmail.com>
2013-05-04 23:05:08 +0200

    Add assets compile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,16 @@ SaveyDashboard::Application.configure do
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = true
+
+
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
