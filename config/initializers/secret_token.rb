ed3e31d6c59e539d36f010637becefa6f78f7979
sirnicolaz <sirnicolaz@gmail.com>
2013-05-04 17:44:35 +0200

    Dashboard born

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaveyDashboard::Application.config.secret_token = '882ffa10ddbf5fe8fceef00b40bd755d577798fbaa2fbe4d91a2942766d35ea4996d3c80583e613333723b9285dae3ab5ec43f9a300dbc1c4670b6d903a51ef1'
